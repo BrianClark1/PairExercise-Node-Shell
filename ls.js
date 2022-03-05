698ce542b36a1383adfcfb6d62944d4bf1a3e049
Brian Clark <briansclark1@gmail.com>
2022-03-05 18:49:37 -0500

    Added ls functionality

--- ls.js
+++ ls.js
@@ -11,10 +11,8 @@ const fs = require("fs");
 //     }
 //     })
 
-
-
 module.exports = function () {
-      process.stdout.write(
+      // process.stdout.write(
           fs.readdir("./", "utf8", (err, files) => {
           if (err) {
             throw err;
@@ -24,32 +22,7 @@ module.exports = function () {
             process.stdout.write("\nprompt > ");
           }
         })
-      );
+      // );
   };
 
 
-
-
-
-module.exports = function () {
-  process.stdout.write("prompt > ");
-
-  process.stdout.on("data", (data) => {
-    const cmd = data.toString().trim();
-    if (cmd === "ls") {
-      console.log(
-        fs.readdir("./", "utf8", (err, files) => {
-          if (err) {
-            throw err;
-          } else {
-            process.stdout.write(files.join("\n"));
-            process.stdout.write("\nprompt > ");
-          }
-        })
-      );
-    } else {
-      process.stdout.write("You typed: " + cmd);
-      process.stdout.write("\n prompt > ");
-    }
-  });
-};
